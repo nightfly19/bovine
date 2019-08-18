c7148eec3421d128b344bdf69bc041f9d98c4e14
Sage Imel <sage@sagenite.net>
2019-08-17 22:51:35 -0700

    Start a simple RPC framework

---
+++ src/lib.rs
@@ -0,0 +1,78 @@
+extern crate bincode;
+extern crate serde;
+
+#[derive(Debug)]
+pub struct SendError<T>(pub T);
+
+#[derive(Debug)]
+pub struct RecvError;
+
+pub trait Sender<T> {
+    fn send(&mut self, t: T) -> Result<(), SendError<T>>
+        where T: serde::Serialize;
+}
+
+pub trait Receiver<T> {
+    fn recv(&mut self) -> Result<T, RecvError>
+        where for<'de> T: serde::Deserialize<'de>;
+}
+
+pub struct ChildProcessChannel<T> {
+    pub child_process: std::process::Child,
+    resource_type: std::marker::PhantomData<T>,
+}
+
+impl<T> ChildProcessChannel<T> {
+    pub fn new (child_process: std::process::Child) -> Option<ChildProcessChannel<T>> {
+        Some(ChildProcessChannel {
+            child_process: child_process,
+            resource_type: std::marker::PhantomData,
+        })
+    }
+}
+
+impl<T> Receiver<T> for ChildProcessChannel<T> {
+    fn recv(&mut self) -> Result<T, RecvError>
+        where for<'de> T: serde::Deserialize<'de>
+    {
+        let stdout_option = self.child_process.stdout.as_mut();
+        match stdout_option {
+            None => {
+                Err(RecvError)
+            },
+            Some(stdout) => {
+                let answer: Result<T,std::boxed::Box<bincode::ErrorKind>> = bincode::deserialize_from(stdout);
+                match answer {
+                    Ok(recv_value) => {
+                        Ok(recv_value)
+                    }
+                    Err(_) => {
+                        Err(RecvError)
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl<T> Sender<T> for ChildProcessChannel<T> {
+    fn send(&mut self, t: T) -> Result<(), SendError<T>>
+        where T: serde::Serialize
+    {
+        let stdin_option = self.child_process.stdin.as_mut();
+        match stdin_option {
+            None => {
+                Err(SendError(t))
+            },
+            Some(stdin) => {
+                match bincode::serialize_into(stdin, &t) {
+                    Ok(_) =>
+                        Ok(()),
+                    Err(_) => {
+                        Err(SendError(t))
+                    }
+                }
+            }
+        }
+    }
+}
